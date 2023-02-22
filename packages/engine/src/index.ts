665fb93616d39f5080ea62cf9b432c8a58f82cd8
Nicky Nickell <pnqcad@gmail.com>
2023-02-22 14:32:53 -0500

    Add websocket upgrade in engine

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -1,18 +1,30 @@
 import express from 'express';
-import * as trpcExpress from '@trpc/server/adapters/express';
+import ws from 'ws';
+import { applyWSSHandler } from '@trpc/server/adapters/ws';
 import { appRouter } from './routers/root';
 import { logger } from './utils/logger';
 
 const app = express();
+const wsServer = new ws.Server({ noServer: true });
 
-app.use(
-  '/',
-  trpcExpress.createExpressMiddleware({
-    router: appRouter
-  })
-);
+const handler = applyWSSHandler({ wss: wsServer, router: appRouter });
+
+app.get('/health', (req, res) => {
+  res.send('healthy');
+});
 
 // TODO: this should be pulled from the environment.
-app.listen(9001, () => {
+const server = app.listen(9001, () => {
   logger.info('Listening on port 9001');
+});
+server.on('upgrade', (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit('connection', socket, request);
+  });
+});
+
+process.on('SIGTERM', () => {
+  handler.broadcastReconnectNotification();
+  server.close();
+  wsServer.close();
 });
